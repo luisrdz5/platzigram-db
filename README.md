b171fb63826a2f1a0bab201c5a40b89578f8ad90
Luis Rodriguez <luis_rdz5@hotmail.com>
2017-02-03 18:31:53 -0600

    Se agregaron las validaciones para la api que estamos desarrollando con test para los hashtags.

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ AVA: Libreria para hacer Test
 
 - Se agrego standard-linter  al proyecto 
 - Se instalo AVA para poder hacer test del codigo
+- Se agregaron las validaciones para la api que estamos desarrollando con test para los hashtags.
--- test/utils-test.js
+++ test/utils-test.js
@@ -1,13 +1,26 @@
 'use strict'
+
 const test = require('ava')
-test('this should pass', t => {
-  t.pass()
-})
-test('esto deberia fallar', t => {
-  t.fail()
-})
-test('it should support async/await', async t => {
-  let p = Promise.resolve(42)
-  let secret = await p
-  t.is(secret, 42)
+const utils = require('../lib/utils')
+
+test('extracting hashtags from text', t => {
+  let tags = utils.extractTags('a #picture with tags #AwesOme #Platzi #ava #100 ##yes')
+
+  t.deepEqual(tags, [
+    'picture',
+    'awesome',
+    'platzi',
+    'ava',
+    '100',
+    'yes'
+  ])
+
+  tags = utils.extractTags('a picture with no tags')
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags()
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags(null)
+  t.deepEqual(tags, [])
 })
